3af75e2550f884bbc0a97ad6bf79030ad47a0dae
AGOODNY <3250594932@qq.com>
2026-04-28 22:57:04 +0800

    some update

--- apps/worker/run_worker.py
+++ apps/worker/run_worker.py
@@ -1,8 +1,14 @@
 import os
+import sys
 import django
 
-# 初始化 Django 环境
+# 把项目根目录加入 Python 路径
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(BASE_DIR)
+
+# 指定 Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 django.setup()
 
 from apps.worker.tasks import start_worker
